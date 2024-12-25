240b6279f16a9be2111aa222cc4b7a3d606aacc0
Gaham <glebsemenov1mail.ru@gmail.com>
2024-12-25 13:41:48 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ function lang(){
   if (l == "ti! (placeholderlang)") { ti() }
 }
 function ti() {
-  o=""
-  c=document.getElementById("code").value
-  if (c=="ti!") { o += "Hello, world!\n" }
+  o = ""
+  c = document.getElementById("code").value
+  if (c == "ti!") { o += "Hello, world!\n" }
   document.getElementById("output").innerHTML = "output:\n" + o
 }
