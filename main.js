936f8b364c2526a3c8a878c9d4e5d80041a3dd8d
Gaham <glebsemenov1mail.ru@gmail.com>
2024-12-27 18:57:04 +0300

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,12 @@ function ti() {
   if (c == "ti!") { o += "Hello, world!\n" } else { o += "[]\n" }
   document.getElementById("o").innerHTML = "output:<br>" + o
 }
+function Perma(code, input, lang) {
+            baseUrl = window.location.origin + window.location.pathname; // Full base URL
+            params = new URLSearchParams({
+                code: encodeURIComponent(code),
+                input: encodeURIComponent(input),
+                lang: encodeURIComponent(lang)
+            });
+            return `${baseUrl}?${params.toString()}`;
+        }
