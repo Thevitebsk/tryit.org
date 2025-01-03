91a74da5a14acbba634081292ba517926b4d4b60
Gaham <glebsemenov1mail.ru@gmail.com>
2025-01-03 23:45:51 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 function langs() {
+  o = ""
   l = document.getElementById("l").value
+  c = document getElementById("c").value
   if (l == "ti! (placeholderlang)") { ti() }
   else if (l == "track!") { track() }
   document.getElementById("o").innerHTML = "output:<br>" + o
 }
 function ti() {
-  o = ""
-  c = document.getElementById("c").value
   if (c == "ti!") { o += "Hello, world!\n" } else { o += "[]\n" }
 }
 function track() { o = "incomplete!" }
