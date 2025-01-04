e4be53f7862126f0cd3cbde9adacc8340be1c3af
Gaham <glebsemenov1mail.ru@gmail.com>
2025-01-04 12:51:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 function langs() {
   o = ""
   l = document.getElementById("l").value
-  c = document getElementById("c").value
+  c = document.getElementById("c").value
   if (l == "ti! (placeholderlang)") { ti() }
-  else if (l == "track!") { track() }
+  else if (l == "EC") { ec() }
   document.getElementById("o").innerHTML = "output:<br>" + o
 }
 function ti() {
   if (c == "ti!") { o += "Hello, world!\n" } 
   else { o += "[]\n" }
 }
-function track() { o = "Language is currently incomplete\n" }
+function ec() { o = "Implementation is currently incomplete\n" }
