e20b76b5d87a03b7af4c101d7c4d17be5f6a4f1c
Gaham <glebsemenov1mail.ru@gmail.com>
2025-01-04 14:54:18 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 function langs() {
-  o = ""
+  o = "" ; p = 0
   l = document.getElementById("l").value
   c = document.getElementById("c").value
   if (l == "ti! (placeholderlang)") { ti() }
-  else if (l == "EC") { ec() }
+  else if (l == "track!") { ec() }
   document.getElementById("o").innerHTML = "output:<br>" + o
 }
 function ti() {
   if (c == "ti!") { o += "Hello, world!\n" } 
   else { o += "[]\n" }
 }
-function ec() { o = "Implementation is currently incomplete\n" }
+function track() {
+  o = "Implementation is currently incomplete\n"
+}
