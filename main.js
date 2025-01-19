60d6de65d5c6c33eb0aed120bbee784e488cab29
Gaham <glebsemenov1mail.ru@gmail.com>
2025-01-19 22:43:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,13 +11,13 @@ function ti() {
   else { o += "[]\n" }
 }
 function paml() {
-  a = b = 0
-  while (p<c.length){
-         if (c[p]=="+") {a++}
-    else if (c[p]=="*") {a,b=a,b}
-    else if (c[p]=="-") {a--}
-    else if (c[p]=="a") {o += a+" "}
-    else if (c[p]=="b") {o += b+" "}
+  a = b = tv = 0
+  while (p<c.length) {
+         if (c[p]=="+") { a++ }
+    else if (c[p]=="*") { tv=a ; a=b ; b=tv ; tv=0 }
+    else if (c[p]=="-") { a-- }
+    else if (c[p]=="a") { o += a + " " }
+    else if (c[p]=="b") { o += b + " " }
     p++
   }
 }
