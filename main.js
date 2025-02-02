4ff9538ec97bbdfba2a0b6b8adb6624eb737ce66
Gaham <glebsemenov1mail.ru@gmail.com>
2025-02-03 00:02:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -21,9 +21,3 @@ function paml() {
     p++
   }
 }
-function perma() {
-  code=encodeURIComponent(document.getElementById("c").value).replaceAll("=", "_").replaceAll("+", "@").replaceAll("/", "$")
-  input=encodeURIComponent(document.getElementById("i").value).replaceAll("=", "_").replaceAll("+", "@").replaceAll("/", "$")
-  lang=encodeURIComponent(document.getElementById("l").value).replaceAll("=", "_").replaceAll("+", "@").replaceAll("/", "$")
-  document.getElementById("p").innerHTML = "permalink:<br>" + code + input + lang
-}
