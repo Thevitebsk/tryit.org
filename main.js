842d7942d0f1ef696de9879dd3672d6e92444d70
Gaham <glebsemenov1mail.ru@gmail.com>
2025-03-12 23:37:07 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function langs() {
   o = "" ; p = 0
   l = document.getElementById("l").value
   c = document.getElementById("c").value
-  if (l == "ti!") { ti() }
+  //if (l == "ti!") { ti() } will be replaced later
   else if (l == "+*-") { paml() }
   else if (l == "6") { six() }
   document.getElementById("o").innerHTML = "output:<br>" + o
